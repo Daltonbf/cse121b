e5ff1c2e3ea5771e63ac40d48758a9413963ca28
Daltonbf <Daltonbf@byui.edu>
2024-03-06 22:11:16 -0700

    first commit

---
+++ w02_tasks/scripts/w02-task.js
@@ -0,0 +1,17 @@
+const fullName = 'Braden Dalton';
+const currentYear = 2024;
+const profilePicture = 'images/pic_of_me.jpg';
+
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+const favoriteFoods = ['Sushi'];
+
+foodElement.innerHTML = favoriteFoods.join('<br>');
